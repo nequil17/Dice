f454bdd0f47064722aeea4e44baf3760ce476d7f
InnovateBham_13 <InnovateBham_13@Admins-iMac.local>
2017-07-09 15:18:36 -0700

    final push

--- main.js
+++ main.js
@@ -1,40 +1,36 @@
 diceArray = [];
 var btn = document.querySelector('#btn')
+var buttontwo = document.querySelector('#buttontwo')
 
 document.addEventListener('DOMContentLoaded', function() {
 
-
 btn.addEventListener('click', function() {
-    var die = new Die();
-
+    var dice = new Die();
+    diceArray.push(dice);
 });
 
-var Die = function(value) {
+var Die = function() {
     var roll = Math.floor(Math.random()*6 + 1);
     this.value = roll;
     this.div = document.createElement('div');
     this.div.className = "box";
     document.body.append(this.div);
-    this.div.innertext = this.value;
-    
+    this.div.innerText = this.value;
+}
 
+buttontwo.addEventListener('click', function() {
+    for(var i = 0; i < diceArray.length; i++) {
+        diceArray[i].roll();
+    }
+});
+
+Die.prototype.roll = function() {
+    this.value = Math.floor(Math.random()*6 +1); 
+    this.div.innerText = this.value;
 }
 
 });
 
-
-
-
-// function roll() {
-//     diceArray.push(new Die());
-// }
-
-
-
-// }Die.prototype.printBox = function() {
-//     return 
-
-// var dieLoop = new Die();
 
 
 
